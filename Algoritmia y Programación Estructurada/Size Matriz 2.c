a31f1c8f1e5fca7808367ed9d33882f1fabe79c5
Luis Alfredo Castañeda Fernandez <54600631+alfredsfdz@users.noreply.github.com>
2019-10-22 10:58:34 -0500

    Add files via upload

---
+++ Algoritmia y Programación Estructurada/Size Matriz 2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main (){
+
+// Declaracion
+int arreglo [12][89];
+
+// Tamaño de la matriz
+int r = sizeof(arreglo)/sizeof(arreglo[0]);
+int c = sizeof(arreglo[0])/sizeof(int);
+
+printf("%d\t",r);
+printf("%d\t \n",c);
+
+// Asignacion
+double arreglo2[r][c];
+int x, y;
+
+for (x = 0; x < r; x++){
+        for (y = 0; y < c; y++){
+            arreglo2[x][y] = (double)(x*y)/12;
+        }
+
+}
+
+printf("%f",arreglo2[5][10]);
+
+    return 0;
+}
